65629cb0cdd6e4f8d5efb589a23e4bac3bc125f2
pipu-kun <SideActivity@proton.me>
2026-03-03 19:58:38 +0700

    menambahkan prosedur dan fungsi

--- file2.cpp
+++ file2.cpp
@@ -1,2 +1,11 @@
 #include <iostream>
 using namespace std;
+
+int p, l;
+
+void input(){
+    cout << "masukan panjang: " << endl;
+    cin >> p;
+    cout << "masukan lebar: " << endl;
+    cin >> l;
+}
